1d9821def2087d2756f3a35383b604dfdc76d26b
HakiPintolli <155005609+HakiPintolli@users.noreply.github.com>
2024-04-20 23:45:10 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,15 +1,15 @@
 <?php
 session_start();
 
-// Fshi sesionin
+
 session_unset();
 session_destroy();
 
-// Fshi cookie për produktet në shportë
+
 $cookie_name = "cart_products";
 setcookie($cookie_name, "", time() - 3600, "/");
 
-// Kthehu tek faqja e fillimit (index.php)
+
 header("Location: index.php");
 exit;
 ?>
